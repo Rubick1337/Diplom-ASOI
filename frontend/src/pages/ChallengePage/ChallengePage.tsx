e2cc62d77782b38131f378f19be78f83c92d183f
Rubick1337 <poss1337@gmail.com>
2026-01-29 18:35:31 +0300

    feat: add page

---
+++ frontend/src/pages/ChallengePage/ChallengePage.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import React from 'react';
+import ChallengeWorkspace from '@/widgets/ChallengeWorkspace/ChallengeWorkspace';
+
+export default function ChallengePage() {
+    return (
+        <main className="app-main-page">
+            <ChallengeWorkspace />
+        </main>
+    );
+}
